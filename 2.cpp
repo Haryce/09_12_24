e428386c536ea0277b1ccc554981fa7b08f83b0f
Haryce <148765923+Haryce@users.noreply.github.com>
2024-12-09 16:39:56 +0900

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+double StepFinder(double n, int step) {
+	if (step == 0) {
+		return 1;
+	}
+	else if (step < 0) {
+		return 1 / StepFinder(n, -step); //для отрицательной степени
+	}
+	else {
+		return n * StepFinder(n, step - 1);
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double n;
+	int step;
+	cout << "Введите основание: ";
+	cin >> n;
+	cout << "Введите степень: ";
+	cin >> step;
+	double result = StepFinder(n, step);
+	cout << "Результат: " << result << endl;
+	return 0;
+}
