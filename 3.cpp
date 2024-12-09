299d3f0606e3c6dd7ce6dd17beb7e035a0dad623
Haryce <148765923+Haryce@users.noreply.github.com>
2024-12-09 16:56:07 +0900

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+void Star(int n, char star) {
+    if (n <= 0) {
+        return; 
+    }
+    cout << "*"; 
+    Star(n - 1, star); 
+}
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    char star = '*';
+    cout << "Введите количество звездочек: ";
+    cin >> n;
+    cout << "Результат: \n";
+    Star(n, star);
+    cout << endl;
+    return 0;
+}
